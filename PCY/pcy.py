6a3a99c7b0b083426e1a39e13c3bcb5b6e7f561e
Ritesh Kasat <rkasat@itswphb34-14.ics.usc.edu>
2015-09-25 21:33:07 -0700

    added logic to generate hashtable

--- PCY/pcy.py
+++ PCY/pcy.py
@@ -1,5 +1,7 @@
 import sys
 import traceback
+import itertools
+from itertools import combinations
 
 def generateBasketsFromInput(rawdata):
   d={}
@@ -7,30 +9,54 @@ def generateBasketsFromInput(rawdata):
   lines = rawdata.readlines()
   for line in lines:
     line=line.strip().split(",")
-    baskets.append(line)
-    # for ele in line:
-    #   d.setdefault(ele,0)
-    #   d[ele]+=1
+    baskets.append(sorted(line))
   return baskets    
 
-def generateItemCountsTable(inputdata):
-    
+def generateItemCountsTable(baskets):
     d={}
-    
-    for line in inputdata:
+    for line in baskets:
       for ele in line:
         d.setdefault(ele,0)
         d[ele]+=1
     return d
 
-def buildHashTable():
-    print
+def buildHashTable(baskets):
+    hashtable={}
+    for basket in baskets:
+      for subset in itertools.combinations(basket, 2):
+        #-------TODO : REPLACE 20 BY BASKET COUNT-------------
+        key = (ord(subset[0])+ord(subset[1]))%20
+
+        hashtable.setdefault(key,[])
+        if len(hashtable[key])>0:
+          hashtable[key][0]+=1
+        
+        else:
+          hashtable[key].append(1)
+
+        hashtable[key].append(subset)
+
+    for k in hashtable:
+      print (k,hashtable[k])   
+
+    return hashtable 
+
+def getFrequentPairs(hashtable,baskets):
+    candidatepairs=set()
+    for k in hashtable:
+      #-------TODO : REPLACE 4 BY SUPPORT COUNT-------------
+      if hashtable[k]>4:
+        
+
 
 if __name__ == '__main__':
   try:
     inputargs = (sys.argv[1], sys.argv[2], sys.argv[3])
+    support = sys.argv[2]
+    bucket = sys.argv[3]
     inputfile = open(sys.argv[1])
-  
+    
+
   except Exception, err:
     print("Invalid input! Try again!")
     sys.exit()
@@ -40,10 +66,12 @@ if __name__ == '__main__':
     #reading input from file
     d = generateBasketsFromInput(inputfile)
     
-    print d
     #generating item counts table
     itemcounttable = generateItemCountsTable(d)    
-    print itemcounttable
+    hashtable = buildHashTable(d)
+    
+
+
   except Exception, err:
     print(traceback.format_exc())
   
