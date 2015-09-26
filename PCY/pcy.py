011447119fc67bdd42727d09a209c72e52b5c711
Ritesh Kasat <rkasat@itswphb34-14.ics.usc.edu>
2015-09-25 19:57:44 -0700

    adding logic to generate item count

--- PCY/pcy.py
+++ PCY/pcy.py
@@ -1,20 +1,53 @@
 import sys
+import traceback
+
+def generateBasketsFromInput(rawdata):
+  d={}
+  baskets=[]
+  lines = rawdata.readlines()
+  for line in lines:
+    line=line.strip().split(",")
+    baskets.append(line)
+    # for ele in line:
+    #   d.setdefault(ele,0)
+    #   d[ele]+=1
+  return baskets    
+
+def generateItemCountsTable(inputdata):
+    
+    d={}
+    
+    for line in inputdata:
+      for ele in line:
+        d.setdefault(ele,0)
+        d[ele]+=1
+    return d
+
+def buildHashTable():
+    print
 
 if __name__ == '__main__':
   try:
     inputargs = (sys.argv[1], sys.argv[2], sys.argv[3])
-    inputdata = open(sys.argv[1])
-    print inputdata
-
-  except:
-    print "-"*60
-    print "Problem with your command line input. Please try again."
-    print "Hint:"
-    print "1)Check if your Input file name is proper."
-    print "2)Check if you passed 3 arguements to the program." 
-    print "-"*60
+    inputfile = open(sys.argv[1])
+  
+  except Exception, err:
+    print("Invalid input! Try again!")
+    sys.exit()
 
   
+  try:  
+    #reading input from file
+    d = generateBasketsFromInput(inputfile)
+    
+    print d
+    #generating item counts table
+    itemcounttable = generateItemCountsTable(d)    
+    print itemcounttable
+  except Exception, err:
+    print(traceback.format_exc())
+  
+
 
 
 
