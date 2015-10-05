39ac22312422ef2e55c07991a54dc33568a3d771
Ritesh Kasat <riteshkasat@gmail.com>
2015-10-05 11:03:58 -0700

    removed unnecssary files

--- PCY/pcy.py
+++ PCY/pcy.py
@@ -1,92 +1,113 @@
-import sys
-import traceback
 import itertools
+import sys
 from itertools import combinations
 
-def generateBasketsFromInput(rawdata):
-  d={}
-  baskets=[]
-  lines = rawdata.readlines()
-  for line in lines:
-    line=line.strip().split(",")
-    baskets.append(sorted(line))
-  return baskets    
+#takes :  single itemset. For ex [a,b,c]
+#return : bucket to which the itemset is hashed
+def generateHash(singleItemset):
+	total = 41 #intialised with a prime number
+	for item in singleItemset:
+		total+=ord(item)
+	return total%bucketCount
 
-def generateItemCountsTable(baskets):
-    d={}
-    for line in baskets:
-      for ele in line:
-        d.setdefault(ele,0)
-        d[ele]+=1
-    return d
+def generateBitMap(hashTable):
+	bitmap = 0
+	for k,v in hashTable.iteritems():		
+		if v >= int(support):	
+			bitmap = bitmap | 1<<int(k)
+	return bitmap
 
-def buildHashTable(baskets):
-    hashtable={}
-    for basket in baskets:
-      for subset in itertools.combinations(basket, 2):
-        #-------TODO : REPLACE 20 BY BASKET COUNT-------------
-        key = (ord(subset[0])+ord(subset[1]))%20
-
-        hashtable.setdefault(key,[])
-        if len(hashtable[key])>0:
-          hashtable[key][0]+=1
-        
-        else:
-          hashtable[key].append(1)
-
-        hashtable[key].append(subset)
-
-    for k in hashtable:
-      print (k,hashtable[k])   
-
-    return hashtable 
-
-def getFrequentPairs(hashtable,baskets):
-    candidatepairs=set()
-    for k in hashtable:
-      #-------TODO : REPLACE 4 BY SUPPORT COUNT-------------
-      if hashtable[k]>4:
-         for basket in baskets:
-            for subset in itertools.combinations(basket, 2):
-              key = (ord(subset[0])+ord(subset[1]))%20
-              if key==k:
-                candidatepairs.add(subset);
-
-      print candidatepairs
+#takes : list of baskets. For ex: [['a','d'],['f','g']]
+#return : hashTable
+def generateHashTable(baskets, size):
+	hashTable={}
+	for line in baskets:
+		subsets = []
+		for subset in itertools.combinations(line, size):
+	 		subsets.append(subset)
+		
+		for ele in subsets:
+			hashKey = generateHash(ele)
+			hashTable.setdefault(hashKey,0)
+			hashTable[hashKey]+=1	
+	return hashTable
 
 
-        
+def generateFrequentItemsets(baskets,size,bitmap,previousFrequentItemsets):
+	frequentItemsetsCandidates ={}
+	subsets = set()
+	result=[]
+	for line in baskets:
+		for subset in itertools.combinations(line, size):
+	 		subsets.add(subset)
 
+	for subset in subsets:
+		count = 0
+		temp = itertools.combinations(subset, size-1)
+		for item in temp:  
+			if item in previousFrequentItemsets:
+				count+=1
 
+		if bitmapLookup(bitmap,list(subset)) and count==len(list(temp)):	
+			frequentItemsetsCandidates.setdefault(subset,0)
+			
+	for line in baskets:
+		for subset in itertools.combinations(line, size):
+			if subset in frequentItemsetsCandidates.keys():
+				frequentItemsetsCandidates[subset] +=1
+
+	for k,v in frequentItemsetsCandidates.iteritems():
+		if int(v)>=support:
+			result.append(list(k))
+	return sorted(result)
+	
+def bitmapLookup(bitmap,itemSet):
+	hashValue = 1<<int(generateHash(itemSet))	
+	return True if hashValue & bitmap != 0 else False
+
+def generateFrequentSingletons(baskets):
+	d={}
+	result=[]
+	for line in baskets:
+		for ele in line:
+			d.setdefault(ele,0)
+			d[ele]+=1
+	
+	for k,v in d.iteritems():
+		if v>=int(support):
+			result.append(k)
+
+	return sorted(result)
+		
 if __name__ == '__main__':
-  try:
-    inputargs = (sys.argv[1], sys.argv[2], sys.argv[3])
-    support = sys.argv[2]
-    bucket = sys.argv[3]
-    inputfile = open(sys.argv[1])
-    
-
-  except Exception, err:
-    print("Invalid input! Try again!")
-    sys.exit()
-
-  
-  try:  
-    #reading input from file
-    d = generateBasketsFromInput(inputfile)
-    
-    #generating item counts table
-    itemcounttable = generateItemCountsTable(d)    
-    hashtable = buildHashTable(d)
-    getFrequentPairs(hashtable,d)
-    
-
-
-  except Exception, err:
-    print(traceback.format_exc())
-  
-
-
-
-
-
+		support = int(sys.argv[2])
+		bucketCount = int(sys.argv[3])
+		inputfile = open(sys.argv[1])
+		baskets=[]
+		#reading input from file
+		lines = inputfile.readlines()
+		for line in lines:
+			line=line.strip().split(",")
+			baskets.append(sorted(line))
+		
+		size=1; hashTable={}
+		frequentItemsets= generateFrequentSingletons(baskets)
+		bitmap=0;previousFrequentItemsets=[]
+		while len(frequentItemsets)>0 or size==1:
+			if size==1:
+				frequentItemsets= generateFrequentSingletons(baskets)
+				print (frequentItemsets); print
+			else:
+				bitmap = generateBitMap(hashTable)
+				previousFrequentItemsets=frequentItemsets
+				frequentItemsets= generateFrequentItemsets(baskets,size,bitmap,previousFrequentItemsets)
+				print (frequentItemsets); print
+			
+			
+			size +=1
+			hashTable = generateHashTable(baskets, size)
+			if len(frequentItemsets)==0:
+				 	sys.exit()
+			print (hashTable); 
+			
+			
