7d2956938e933f0e34cdbd13f1954a769d87ed54
Ritesh Kasat <riteshkasat@gmail.com>
2015-10-05 12:27:35 -0700

    completed implementation of multihash

--- MultiHash/multihash.py
+++ MultiHash/multihash.py
@@ -1,11 +1,11 @@
-mport itertools
+import itertools
 import sys
 from itertools import combinations
 
 #takes :  single itemset. For ex [a,b,c]
 #return : bucket to which the itemset is hashed
-def generateHash(singleItemset, primeNumber):
-	total = primeNumber #intialised with a prime number
+def generateHash(singleItemset,prime):
+	total = prime #intialised with a prime number
 	for item in singleItemset:
 		total+=ord(item)
 	return total%bucketCount
@@ -19,7 +19,7 @@ def generateBitMap(hashTable):
 
 #takes : list of baskets. For ex: [['a','d'],['f','g']]
 #return : hashTable
-def generateHashTable(baskets, size, primeNumber):
+def generateHashTable(baskets, size, prime):
 	hashTable={}
 	for line in baskets:
 		subsets = []
@@ -27,13 +27,45 @@ def generateHashTable(baskets, size, primeNumber):
 	 		subsets.append(subset)
 		
 		for ele in subsets:
-			hashKey = generateHash(ele, primeNumber)
+			hashKey = generateHash(ele,prime)
 			hashTable.setdefault(hashKey,0)
 			hashTable[hashKey]+=1	
 	return hashTable
 
 
-	def generateFrequentSingletons(baskets):
+def generateFrequentItemsets(baskets,size,bitmap1,bitmap2,previousFrequentItemsets):
+	frequentItemsetsCandidates ={}
+	subsets = set()
+	result=[]
+	for line in baskets:
+		for subset in itertools.combinations(line, size):
+	 		subsets.add(subset)
+
+	for subset in subsets:
+		count = 0
+		temp = itertools.combinations(subset, size-1)
+		for item in temp:  
+			if item in previousFrequentItemsets:
+				count+=1
+
+		if bitmapLookup(bitmap1,list(subset),prime1) and bitmapLookup(bitmap2,list(subset),prime2) and count==len(list(temp)):	
+			frequentItemsetsCandidates.setdefault(subset,0)
+			
+	for line in baskets:
+		for subset in itertools.combinations(line, size):
+			if subset in frequentItemsetsCandidates.keys():
+				frequentItemsetsCandidates[subset] +=1
+
+	for k,v in frequentItemsetsCandidates.iteritems():
+		if int(v)>=support:
+			result.append(list(k))
+	return sorted(result)
+	
+def bitmapLookup(bitmap,itemSet,prime):
+	hashValue = 1<<int(generateHash(itemSet,prime))	
+	return True if hashValue & bitmap != 0 else False
+
+def generateFrequentSingletons(baskets):
 	d={}
 	result=[]
 	for line in baskets:
@@ -46,46 +78,41 @@ def generateHashTable(baskets, size, primeNumber):
 			result.append(k)
 
 	return sorted(result)
-
-
+		
 if __name__ == '__main__':
 		support = int(sys.argv[2])
 		bucketCount = int(sys.argv[3])
 		inputfile = open(sys.argv[1])
 		baskets=[]
+		prime1=31;prime2=103
 		#reading input from file
 		lines = inputfile.readlines()
 		for line in lines:
 			line=line.strip().split(",")
 			baskets.append(sorted(line))
-
+		
 		size=1; hashTable={}
 		frequentItemsets= generateFrequentSingletons(baskets)
 		bitmap=0;previousFrequentItemsets=[]
-		
+		while len(frequentItemsets)>0 or size==1:
+			if size==1:
+				frequentItemsets= generateFrequentSingletons(baskets)
+				print (frequentItemsets); print
+			else:
+				bitmap1 = generateBitMap(hashTable1)
+				bitmap2 = generateBitMap(hashTable2)
 
-		if size==1:
-			frequentItemsets= generateFrequentSingletons(baskets)
-			print (frequentItemsets); print
-			size +=1;	
-		
-		if size==2:
-			bitmap1 = generateBitMap(hashTable1)
-			bitmap2 = generateBitMap(hashTable2)
-			previousFrequentItemsets=frequentItemsets
-			frequentItemsets= generateFrequentItemsets(baskets,size,bitmap1,bitmape2, previousFrequentItemsets)
+				previousFrequentItemsets=frequentItemsets
+				frequentItemsets= generateFrequentItemsets(baskets,size,bitmap1,bitmap2,previousFrequentItemsets)
+				print (frequentItemsets); print
 			
+			size +=1
+			hashTable1 = generateHashTable(baskets, size,prime1)
+			hashTable2 = generateHashTable(baskets, size,prime2)
+
 			if len(frequentItemsets)==0:
-			 	sys.exit()
-			print (frequentItemsets)
-		
-		hashTable1 = generateHashTable(baskets, size,41)
-		hashTable2 = generateHashTable(baskets, size,53)
-		print (hashTable1);
-		print (hashTable2); 
-		if size!=1: 
-			print;
-		size +=1;
+				 	sys.exit()
+			print (hashTable1);print hashTable2
 
-
-	
+			
+			
